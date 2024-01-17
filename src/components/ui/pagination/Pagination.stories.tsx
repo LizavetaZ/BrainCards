991a1612bb41d6dfde8266befedb72b832a5af48
LizavetaZ <vokibzoj@yandex.by>
2024-01-18 00:17:15 +0300

    no frozen lockfile

--- src/components/ui/pagination/Pagination.stories.tsx
+++ src/components/ui/pagination/Pagination.stories.tsx
@@ -1,51 +1,34 @@
 import { useState } from 'react'
 
-import { Pagination, PaginationProps } from '@/components/ui/pagination/Pagination'
+import { Pagination } from '@/components/ui/pagination/Pagination'
 import { options } from '@/pages/decks/decksBody/selectorConstants.types'
-import { Meta, StoryObj } from '@storybook/react'
+import { Meta } from '@storybook/react'
 
-const meta: Meta = {
-  argTypes: {},
+const meta = {
   component: Pagination,
   tags: ['autodocs'],
   title: 'Components/Pagination',
-}
+} satisfies Meta<typeof Pagination>
 
 export default meta
 
-type DefaultPaginationComponentStory = StoryObj<PaginationProps>
-export const PaginationComponent: DefaultPaginationComponentStory = args => {
-  const [page, setPage] = useState<number>(args.currentPage)
-  const [pageSize, setPageSize] = useState(args.pageSize)
+export const Defalut = {
+  render: () => {
+    const [pageSize, setPageSize] = useState(10)
+    const [currentPage, setCurrentPage] = useState<number>(1)
 
-  const onChangePageSize = (pageSize: number | string) => {
-    setPageSize(+pageSize)
-  }
-
-  const onPageChange = (page: number | string) => {
-    setPage(+page)
-  }
-
-  return (
-    <>
-      <Pagination
-        availablePageSizes={args.availablePageSizes}
-        currentPage={page}
-        onChangePageSize={onChangePageSize}
-        onPageChange={onPageChange}
-        pageSize={pageSize}
-        siblingCount={args.siblingCount}
-        totalCount={args.totalCount}
-      />
-    </>
-  )
-}
-PaginationComponent.args = {
-  availablePageSizes: options,
-  currentPage: 1,
-  onChangePageSize: () => {},
-  onPageChange: () => {},
-  pageSize: 10,
-  siblingCount: 2,
-  totalCount: 10000,
+    return (
+      <>
+        <Pagination
+          availablePageSizes={options}
+          currentPage={currentPage}
+          onChangePageSize={pageSize => setPageSize(+pageSize)}
+          onPageChange={(currentPage: number | string) => setCurrentPage(+currentPage)}
+          pageSize={pageSize}
+          siblingCount={2}
+          totalCount={10000}
+        />
+      </>
+    )
+  },
 }
